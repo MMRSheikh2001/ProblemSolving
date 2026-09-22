22409153832b9401640b03df83107f3e682faeaf
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-23 04:21:38 +0600

    add

---
+++ SirGivenProblems/ArrayBased/FindDuplicate.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        int[] arr = { 23, 12, 54, 34, 87, 4, 56, 90, 65, 45, 12, 24, 34 };
+
+        System.out.println(findDuplicates(arr));
+    }
+
+    public static List<Integer> findDuplicates(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+            set.add(arr[i]);
+        }
+
+        for (Integer n : set) {
+            list.remove(list.indexOf(n));
+        }
+
+        return list;
+    }
+}
