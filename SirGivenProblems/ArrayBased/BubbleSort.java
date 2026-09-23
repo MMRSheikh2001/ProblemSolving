39d8ef9d90938f532a8375c39370e1fc65fdcf51
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-23 21:28:38 +0600

    add

--- SirGivenProblems/ArrayBased/BubbleSort.java
+++ SirGivenProblems/ArrayBased/BubbleSort.java
@@ -1,10 +1,23 @@
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        
+        int[] arr = { 23, 12, 54, 34, 87, 4, 56, 90, 65, 45, 12, 24, 34 };
+
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+
     }
-    public static int[] bubbleSort(int[] arr){
-        for(int i=0;i<arr.length-1;i++){
-            
+
+    public static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    arr[j] = arr[j] + arr[j + 1];
+                    arr[j + 1] = arr[j] - arr[j + 1];
+                    arr[j] = arr[j] - arr[j + 1];
+                }
+            }
         }
+        return arr;
     }
 }
---
+++ SirGivenProblems/ArrayBased/SelectionSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 23, 12, 54, 34, 87, 4, 56, 90, 65, 45, 12, 24, 34 };
+
+        System.out.println(Arrays.toString(selectionSort(arr)));
+
+    }
+
+    public static int[] selectionSort(int[] arr) {
+
+      
+
+        return arr;
+
+    }
+}
