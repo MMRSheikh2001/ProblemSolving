0e5ea0f95ab4f6a3a833e930ca90325c6f833f22
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-24 18:19:14 +0600

    add

--- SirGivenProblems/ArrayBased/RemoveDuplicates.java
+++ SirGivenProblems/ArrayBased/RemoveDuplicates.java
@@ -1,10 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        
-    }
-    private static int[] removeDuplicates(int[] arr){
 
-        
+        int[] arr = { 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 11, 13, 12, 14, 10, 12, 13, 14, 15, 16 };
+
+        System.out.println(Arrays.toString(removeDuplicates(arr)));
+
+    }
+
+    private static int[] removeDuplicates(int[] arr) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (!list.contains(arr[i])) {
+
+                list.add(arr[i]);
+            }
+
+        }
+
+        arr = new int[list.size()];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
 
         return arr;
     }
