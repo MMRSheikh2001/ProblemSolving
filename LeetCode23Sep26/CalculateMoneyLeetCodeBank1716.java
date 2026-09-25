418aaade8706297d192622d10287d50dc7ab33c9
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-25 20:50:09 +0600

    add

---
+++ LeetCode23Sep26/CalculateMoneyLeetCodeBank1716.java
@@ -0,0 +1,15 @@
+public class CalculateMoneyLeetCodeBank1716 {
+    public static void main(String[] args) {
+        
+    }
+}
+class Solution {
+    public int totalMoney(int n) {
+        int totalMoney=0;
+
+        
+
+        return totalMoney;
+        
+    }
+}
